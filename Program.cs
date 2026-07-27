77297098e6b4fbb2aa66f1a817cf4ad6fd65f63a
Brajan EM <estrada.brajan@gmail.com>
2026-07-27 11:10:25 -0300

    Primer commit: estructura inicial del proyecto

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PrimerProyecto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
